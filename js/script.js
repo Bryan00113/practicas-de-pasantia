4f1fbdcec7137abd51e49fc7b2f5a4334c3d295a
Bryan Muñoz Gutierrez <bryanpotoso99@gmail.com>
2023-10-12 00:37:48 -0600

    practica menu con tiempo de accion

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-const enlace1 = document.querySelector(".enlace1");
-const enlace2 = document.querySelector(".enlace2");
-const enlace3 = document.querySelector(".enlace3");
-
-const ulnesting1 = document.querySelector(".ul_nesting1");
+const ulnesting1 = document.querySelector(".ul_nesting1 ");
 const ulnesting2 = document.querySelector(".ul_nesting2");
 const ulnesting3 = document.querySelector(".ul_nesting3");
 
-// enlace1.onclick = () => {
-//   ulnesting1.classList.remove("activo");
-// };
-enlace1.addEventListener("click", () => {
-  ulnesting1.classList.toggle("activo");
+document.querySelector(".enlace1").addEventListener("click", function () {
+  setTimeout(function () {
+    ulnesting1.classList.toggle("activo");
+  }, 2000);
 });
-enlace2.addEventListener("click", () => {
-  ulnesting2.classList.toggle("activo");
+document.querySelector(".enlace2").addEventListener("click", function () {
+  setTimeout(function () {
+    ulnesting2.classList.toggle("activo");
+  }, 2000);
 });
-enlace3.addEventListener("click", () => {
-  ulnesting3.classList.toggle("activo");
+document.querySelector(".enlace3").addEventListener("click", function () {
+  setTimeout(function () {
+    ulnesting3.classList.toggle("activo");
+  }, 2000);
 });
